70617014e660756431ff7cc784eb128283ee4e19
nikhilgoswami <nikhilgoswamikeeda@gmail.com>
2026-04-11 10:42:53 +0530

    Basics of Python Language , Rest I'll cover by practising  :)

---
+++ Python/Basics/list_sets_tuples_dictionary.py
@@ -0,0 +1,36 @@
+# TUPLES -  ORDERED IMMUTABLE
+# They are immutable so a few methods are availaible for this data type :
+# len(tup) - Returns length of the tuple
+# tup1 + tup1 - Return single tuple which contains elements of both the tuples
+# min(tup) and max(tup) - Return minimum/maximum element of tuple
+# tup1*3 + tup2  - (1,2,1,2,3,4)
+
+# LISTS - ORDERED MUTABLE
+# Lists are mutable and contains many functionalities
+# list.append() - Add element/list to the last of list 
+# list.pop() - Removes and returns the element
+# list.insert(index,element) - Add value to a particular index
+# list.sort() or list.sort(reverse=True) - Sorts the list
+# list.extend(list2) - Adds the elements of another list 
+# list.remove(el)
+# list.reverse()
+# min/max(list)
+# list.index(element) - .find is a string method , not list method 
+# enumerate(list) - Access both index and element at the same time 
+
+# SETS - UNORDERED 
+# set.difference(s2)  - Elements in s2 which are not present in s1
+# set.union(s2)   - Adding two sets
+# set.intersection(s2)  - Common of two sets
+# set.update([el1,el2,el3])  - To add multiple elements
+# set.remove(1) - To remove elements
+# set.add("Hello") - For adding single element
+
+# DICTIONARY = UNORDERED , MUTABLE
+# dic.keys() - Shows all the keys
+# dic.values()  - Shows all the values
+# dic.items() - Shows both keys and values
+# dic.update.dic2 or directly update({ }) - Updates the dictionary
+# dic.get('key','Not found') - Safe method to check/return keys/elements
+# dic.pop('age') or del dic ['age']  - Deletes key value pair
+
---
+++ Python/Basics/numeric.py
@@ -0,0 +1,8 @@
+num = 3.6345
+print(abs(num))
+print(round(num,2))
+
+
+# abs()  or absoulte method is used to return absolute/true value of a number 
+# round() method is simply used for rounding off the number to nearest integer.
+# round() method used Banker's rounding technique
